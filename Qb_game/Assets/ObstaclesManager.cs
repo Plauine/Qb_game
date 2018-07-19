7d82e73ce2a2af6e1afbaad8dfe5ead0c498a354
Pauline Baudouin <paulinebaudouin2@gmail.com>
2018-07-19 11:00:50 +0200

    Ooops, I forgot to commit...

---
+++ Qb_game/Assets/ObstaclesManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesManager : MonoBehaviour {
+
+    public GameObject obstacle;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start");
+        InvokeRepeating("generateObstacles", spawnTime, spawnTime);
+	}
+
+    void generateObstacles(){
+        for (int i = 0; i < spawnPoints.Length; i++){
+            int spawn = Random.Range(0, 2);
+            Debug.Log(i + " - " + spawn);
+            if(spawn == 1){
+                
+                //Instantiate(obstacle, spawnPoints[i].position, spawnPoints[i].rotation);
+            }
+        }
+    }
+}
---
+++ Qb_game/Assets/PlayerMovements.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float jumpForce;
+    public float sideForce;
+
+    private bool canJump = false;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    // FixedUpdate is called once per frame to manage physics
+    void FixedUpdate () {
+        if(Input.GetKey("q")){
+            rb.AddForce(- sideForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("space") && canJump)
+        {
+            rb.AddForce(0, jumpForce * Time.deltaTime, 0);
+            canJump = false;
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+}
